fefce763c7b92e6c1df9621216260747a703ae09
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-02 12:14:10 -0500

    starting DDD arch with domain and create access token

---
+++ src/domain/access_token/access_token.go
@@ -0,0 +1,27 @@
+package access_token
+
+import "time"
+
+const (
+	expirationTime = 24
+)
+
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserID      int64  `json:"user_id"`
+	ClientID    int64  `json:"client_id"`
+	Expires     int64  `json:"expires"`
+}
+
+func GetNewAccessToken() AccessToken {
+	return AccessToken{
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
+
+func (at AccessToken) IsExpired() bool {
+	return false
+}
+
+// Web frontend - Client-Id: 123
+// Android App - Client-ID: 234
