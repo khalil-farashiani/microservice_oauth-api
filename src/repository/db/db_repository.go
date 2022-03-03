ec2b397e741b43ececa6d260197935b09f008a1a
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-03 08:40:15 -0500

    add repository dir to intract with cassandra DB

---
+++ src/repository/db/db_repository.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"github.com/khalil-farashiani/microservice_oauth-api/src/domain/access_token"
+	"github.com/khalil-farashiani/microservice_oauth-api/src/utils/errors"
+)
+
+func NewRepository() DbRepository {
+	return &dbRepository{}
+}
+
+type DbRepository interface {
+	GetByID(string) (*access_token.AccessToken, *errors.RestErr)
+}
+
+type dbRepository struct {
+}
+
+func (r *dbRepository) GetByID(string) (*access_token.AccessToken, *errors.RestErr) {
+	return nil, nil
+}
