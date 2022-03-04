528d39625cf0f571f37fb08b48cd49a1711e03ff
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-04 10:43:56 -0500

    change in start app

--- src/app/application.go
+++ src/app/application.go
@@ -1,10 +1,19 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/khalil-farashiani/microservice_oauth-api/src/domain/access_token"
+	"github.com/khalil-farashiani/microservice_oauth-api/src/http"
 	"github.com/khalil-farashiani/microservice_oauth-api/src/repository/db"
 )
 
+var (
+	router = gin.Default()
+)
+
 func StartApplication() {
-	atService := access_token.NewService(db.NewRepository())
+	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+	router.Run(":8080")
 }
